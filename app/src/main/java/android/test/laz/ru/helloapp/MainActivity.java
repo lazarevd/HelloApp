3201fc351faa6af445ee9a2310a5cc61bb0654d6
Dmitry Lazarev <lazarevd@gmail.com>
2017-03-30 00:23:10 +0300

    Новые кнопки

--- app/src/main/java/android/test/laz/ru/helloapp/MainActivity.java
+++ app/src/main/java/android/test/laz/ru/helloapp/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import com.android.volley.RequestQueue;
 
 
 
